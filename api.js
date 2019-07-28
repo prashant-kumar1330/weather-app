95e3c09ed4cb0c225feff58afff92ddc42650f0d
prashant kumar <kumarprashant1330@gmail.com>
2019-07-28 15:26:01 +0530

    api added

---
+++ api.js
@@ -0,0 +1,13 @@
+class openweather{
+
+constructor(){
+    this.api='8bf8cf609cba55933164bbea08f37bf5';
+}
+ async search(cityName){
+     console.log(cityName);
+     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${this.api}`)
+    const result= await response.json();
+    console.log(result);
+   
+}
+}
--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//search for city
- 
-// take the user input
+const weather = new openweather();
 window.onload=function(){
+   
+    //search for city
     var searchCity= document.getElementById("searchbar")
     searchCity.addEventListener("keyup",(event)=>{
-        const usertext= event.target.value;
+        // take the user input
+        const cityName = event.target.value;
+        //search for the weather
+        weather.search(cityName);
         
-        console.log(usertext);
+       
     })
 }
